1bf19540f8cfbdc0f0114a762bbc1a44e778380b
Cooks-Johns <cooks.johns@gmail.com>
2021-02-12 17:48:46 -0600

    fixing spawn order

--- screeps/src/main.js
+++ screeps/src/main.js
@@ -5,7 +5,7 @@ var roleBuilder = require('role.builder');
 module.exports.loop = function () {
 
 
-// 	TOWER
+// ---> need to fix	TOWER
     var tower = Game.getObjectById('4f171b5f9ca4e3c11dd9f02b');
     if(tower) {
         var closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
@@ -29,37 +29,40 @@ module.exports.loop = function () {
             console.log('Clearing non-existing creep memory:', name);
         }
     }
-	
-// RESPWAN - builders
-    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
-    console.log('Builder: ' + builders.length);
-
-    if(builders.length < 5) {
-        var newName = 'Builder' + Game.time;
-        console.log('Spawning new builder: ' + newName);
-        Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE, MOVE], newName,
-            {memory: {role: 'builder'}});
-    }
-// RESPWAN - harvesters
-	var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
-		console.log('Harvester: ' + harvesters.length);
-
-		if(harvesters.length < 3) {
-			var newName = 'Harvester' + Game.time;
-			console.log('Spawning new harvester: ' + newName);
-			Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], newName,
-				{memory: {role: 'harvester'}});
-		}
-// RESPWAN - upgraders
+    
+    
+    // RESPWAN - upgraders
 	var upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
 		console.log('Upgrader: ' + upgraders.length);
 
-		if(upgraders.length < 1) {
+		if(upgraders.length < 2) {
 			var newName = 'Upgrader' + Game.time;
 			console.log('Spawning new upgrader: ' + newName);
 			Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE, MOVE], newName,
 				{memory: {role: 'upgrader'}});
 		}
+    
+    // RESPWAN - harvesters
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    	console.log('Harvester: ' + harvesters.length);
+    
+    if(harvesters.length < 4) {
+		var newName = 'Harvester' + Game.time;
+		console.log('Spawning new harvester: ' + newName);
+		Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, CARRY, MOVE, MOVE], newName,
+			{memory: {role: 'harvester'}});
+	}
+	
+// RESPWAN - builders
+    var builders = _.filter(Game.creeps, (creep) => creep.memory.role == 'builder');
+    console.log('Builder: ' + builders.length);
+
+    if(builders.length < 10) {
+        var newName = 'Builder' + Game.time;
+        console.log('Spawning new builder: ' + newName);
+        Game.spawns['Spawn1'].spawnCreep([WORK, WORK, CARRY, MOVE, MOVE], newName,
+            {memory: {role: 'builder'}});
+    }
 
 
 // CREEP SPAWNING LOCATION 
